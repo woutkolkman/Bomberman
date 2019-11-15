991ff10f743a44b0776466baf5428d983ecd6873
woutkolkman <wout.kolkman@hotmail.com>
2019-11-15 17:50:33 +0100

    +ir library placeholder & ADC uitgebreid

---
+++ libraries/IR/ir.c
@@ -0,0 +1,14 @@
+/*
+ *	IR communication library
+ */
+
+/* includes */
+#include "ir.h"
+
+/* defines */
+// ...
+
+/* functions */
+void voorbeeld_functie(void) {
+	
+}
---
+++ libraries/IR/ir.h
@@ -0,0 +1,24 @@
+/*
+ *	IR communication library
+ */
+ 
+// voorkom meerdere keren includen
+#ifndef IR_H
+#define IR_H
+
+/* includes */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+ 
+/* function prototypes */
+void voorbeeld_functie();
+ 
+#ifdef __cplusplus
+}
+#endif
+ 
+#endif // IR_H
