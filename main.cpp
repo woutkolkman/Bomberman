9c4d1af07258955b8a16b1c22eb3a9e31fd31f11
Stefan <stefan221100@gmail.com>
2019-11-18 14:19:15 +0100

    IR LED timers setup compleet

--- main.cpp
+++ main.cpp
@@ -3,13 +3,14 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "libraries/IR/ir.h" // IR library
-#include "libraries/IRremote-2.2.3/IRremote.h" // testen van bestaande library
 // ... // LCD library
 
+// Hallo dit is Nadia
 
 /* defines and global variables */
 volatile uint8_t brightness = 0;
-
+#define HALFDUTYCYCLE 128;
+#define KHZ38 421;
 
 /* function prototypes */
 void adc_init();
@@ -17,6 +18,12 @@ void init();
 
 
 /* ISR */
+
+ISR(TIMER1_COMPA_vect) {
+	TCCR2A ^= (1<<COM2A1); //toggle timer2 fastPWM/off
+}
+
+
 ISR(ADC_vect) { // wordt aangeroepen wanneer ADC conversie klaar is
 	brightness = (ADC>>2); // 10 bits, gooi 2 LSB weg, uitkomst 8 bits
 
@@ -30,7 +37,7 @@ int main(void) {
 
 
 	/* loop */
-	for(;;) {
+	while(1) {
 		
 	}
 
@@ -62,24 +69,25 @@ void init() {
 	 */
 	DDRD |= (1<<DDD3); // IR-zender
 	DDRD &= ~(1<<DDD2); //IR-ontvanger
-	DDRBD |= (1<<DDB1) | (1<<DDB2) | (1<<DDB3) | (1<<DDB4) | (1<<DDB5); // TFT scherm
+	DDRB |= (1<<DDB1) | (1<<DDB2) | (1<<DDB3) | (1<<DDB4) | (1<<DDB5); // TFT scherm
 	DDRD |= (1<<DDD0) | (1<<DDD1); //UART
 
 	sei(); // set global interrupt flag
 }
 
 void timer0_init() {
-	
 }
 
 void timer1_init() {
-	
+	TCCR1B |= (1<<WGM12); //CTC mode
+	TCCR1B |= (1<<CS10); //no prescaling
+	OCR1A = KHZ38;
 }
 
 void timer2_init() {
 	TCCR2A |= (1<<WGM20) | (1<< WGM21); //fast PWM mode
-	TCCR2B |= (1<<CS20);
-	
+	TCCR2B |= (1<<CS20); //no prescaling
+	OCR2A = HALFDUTYCYCLE;
 }
 
 void adc_init() { // initialiseer ADC
@@ -88,7 +96,7 @@ void adc_init() { // initialiseer ADC
 	ADCSRA |= (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0); // ADC clock prescaler ...(nog kiezen)
 
 	ADCSRA |= (1<<ADATE); // ADC auto trigger enable \/
-	ADCSRB &= ~(1<<ADTS2) | ~(1<<ADTS1) | ~(1<<ADTS0); // free running mode
+	ADCSRB &= ~(1<<ADTS2) & ~(1<<ADTS1) & ~(1<<ADTS0); // free running mode
 
 	ADCSRA |= (1<<ADEN); // enable ADC
         ADCSRA |= (1<<ADSC); // start eerste meting
