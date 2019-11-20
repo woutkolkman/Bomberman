ed9883a73c64b860d07e648dea3450d4b18edfca
Stefan <stefan221100@gmail.com>
2019-11-20 17:01:32 +0100

    Frequentie hersteld, beperkt tot 1 timer

--- libraries/IR/ir.c
+++ libraries/IR/ir.c
@@ -5,10 +5,12 @@
 /* defines */
 // ...
 //#define KHZ38 421 timer1 prescale 1
-#define KHZ38 53 //timer0 prescale 8
+#define KHZ38 52 //timer2 prescale 8
+#define DUTYCYCLE38 26 //duty cycle 50% 38KHz
 //#define KHZ56 286 timer1 prescale 1
-#define KHZ56 36 //timer0 prescale 8
-#define HALFDUTYCYCLE 128
+#define KHZ56 36 //timer2 prescale 8
+#define DUTYCYCLE56 18 //duty cycle 50% 56KHz
+#define CLEAR 0; //clear value for OCR2B
 #define STARTBITVALUE //INVULLEN, kleiner of gelijk aan 65535 (wat veel te groot is)
 
 /* includes */
@@ -18,27 +20,31 @@
 //...
 
 /* functions */
-ISR(TIMER0_COMPA_vect) {
-	if(OCR2B) {
-		OCR2B = 0; //LED off
-	} else {
-	OCR2B = HALFDUTYCYCLE; //50% duty cycle
-	}
-}
 
 void IR_prepare_timer_send(uint8_t frequency) {
-	TCCR0A |= (1<<WGM01); //CTC mode
-	TCCR0B |= (1<<CS01); //prescale 8
-	//TCCR1B |= (1<<WGM12); //CTC mode
-	//TCCR1B |= (1<< CS10); //no prescaling
+	TCCR2A |= (1<<WGM20) | (1<<WGM21);
+	TCCR2B |= (1<<WGM22) //CTC, fast PWM
+	TCCR2A |= (1<<COM2B1); //set above value, clear on bottom
 	if (frequency == 38) {
-		OCR0A = KHZ38;
+		OCR2A = KHZ38;
+		OCR2B = DUTYCYCLE38;
 	} else if (frequency == 56) {
-		OCR0A = KHZ56;
+		OCR2A = KHZ56;
+		OCR2B = DUTYCYCLE56;
 	}
-	TCCR2A |= (1<<WGM20) | (1<< WGM21); //fast PWM mode
-        TCCR2B |= (1<<CS20); //no prescaling
-        OCR2B = HALFDUTYCYCLE; //50% duty cycle
+/*	TCCR0A |= (1<<WGM01); //CTC mode
+*	TCCR0B |= (1<<CS01); //prescale 8
+*	TCCR1B |= (1<<WGM12); //CTC mode
+*	TCCR1B |= (1<< CS10); //no prescaling
+*	if (frequency == 38) {
+*		OCR0A = KHZ38;
+*	} else if (frequency == 56) {
+*		OCR0A = KHZ56;
+*	}
+*	TCCR2A |= (1<<WGM20) | (1<< WGM21); //fast PWM mode
+*        TCCR2B |= (1<<CS20); //no prescaling
+*        OCR2B = HALFDUTYCYCLE; //50% duty cycle
+*/
 }
 
 void IR_prepare_timer_receive(void) {
