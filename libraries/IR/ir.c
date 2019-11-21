dd9a4963501ff4bdd06b5cc20da2192102f4768f
Stefan <stefan221100@gmail.com>
2019-11-21 11:10:02 +0100

    overbodige comment weggehaald

--- libraries/IR/ir.c
+++ libraries/IR/ir.c
@@ -10,7 +10,6 @@
 //#define KHZ56 286 timer1 prescale 1
 #define KHZ56 36 //timer2 prescale 8
 #define DUTYCYCLE56 18 //duty cycle 50% 56KHz
-#define CLEAR 0; //clear value for OCR2B
 #define STARTBITVALUE //INVULLEN, kleiner of gelijk aan 65535 (wat veel te groot is)
 
 /* includes */
@@ -32,19 +31,6 @@ void IR_prepare_timer_send(uint8_t frequency) {
 		OCR2A = KHZ56;
 		OCR2B = DUTYCYCLE56;
 	}
-/*	TCCR0A |= (1<<WGM01); //CTC mode
-*	TCCR0B |= (1<<CS01); //prescale 8
-*	TCCR1B |= (1<<WGM12); //CTC mode
-*	TCCR1B |= (1<< CS10); //no prescaling
-*	if (frequency == 38) {
-*		OCR0A = KHZ38;
-*	} else if (frequency == 56) {
-*		OCR0A = KHZ56;
-*	}
-*	TCCR2A |= (1<<WGM20) | (1<< WGM21); //fast PWM mode
-*        TCCR2B |= (1<<CS20); //no prescaling
-*        OCR2B = HALFDUTYCYCLE; //50% duty cycle
-*/
 }
 
 void IR_prepare_timer_receive(void) {
@@ -53,6 +39,7 @@ void IR_prepare_timer_receive(void) {
 
 void IR_send(uint8_t waarde) {
 	// start bit
+
 	while(TCNT1 < STARTBITVALUE)
 		
 	for (int i=7, i<0, i--) {
