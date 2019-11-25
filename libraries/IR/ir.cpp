446956bf10ab03c521df72d94ff38fd1d06fdf62
Stefan <stefan221100@gmail.com>
2019-11-25 14:58:27 +0100

    Overbodige dingen weggelaten
    1 vaste tijdsduur voor beide frequenties ipv 2 tijdsduren (per frequentie 1 tijdsduur)

--- libraries/IR/ir.cpp
+++ libraries/IR/ir.cpp
@@ -6,19 +6,14 @@
 /* defines */
 #define KHZ38 52 //timer2 prescale 8
 #define DUTYCYCLE38 26 //duty cycle 50% 38KHz
-#define TOTALBIT38 20800 //400 keer knipperen
-#define STARTBITVALUE38 16640 //80%
-#define BITIS138 15600 //75%
-#define BITIS038 10400 //50%
-#define STOPBITVALUE38 5200 //25%
+#define TOTALBIT 20800 //totale duur 1 bit
+#define STARTBITVALUE 16640 //80%
+#define BITIS1 15600 //75%
+#define BITIS0 10400 //50%
+#define STOPBITVALUE 5200 //25%
 
 #define KHZ56 36 //timer2 prescale 8
 #define DUTYCYCLE56 18 //duty cycle 50% 56KHz
-#define TOTALBIT56 14400 //400 keer knipperen
-#define STARTBITVALUE56 11520 //80%
-#define BITIS156 10800 //75%
-#define BITIS056 7200 //50%
-#define STOPBITVALUE56 3600 //25%
 
 #define OFFSET 100 // offset waarde voor kleine afwijking
 
@@ -52,36 +47,18 @@ ISR (PCINT2_vect) { // wordt aangeroepen bij logische 1 naar 0 of 0 naar 1 van o
 	if (DDRD & (1<<DDD2)) { // opgaande flank (0 -> 1)
 		// bepaal verschil huidige counterstand en vorige counterstand
 		diffcounter = TCNT1 - prevcounter; //TCNT1 - prevcounter;
-		#if FREQUENCY == 38
-		if (diffcounter >= (STARTBITVALUE38 - OFFSET) && diffcounter <= (STARTBITVALUE38 + OFFSET)) { // startbit
+		if (diffcounter >= (STARTBITVALUE - OFFSET) && diffcounter <= (STARTBITVALUE + OFFSET)) { // startbit
 
-		} else if (diffcounter >= (STOPBITVALUE38 - OFFSET) && diffcounter <= (STOPBITVALUE38 + OFFSET)) { // stopbit
-
+		} else if (diffcounter >= (STOPBITVALUE - OFFSET) && diffcounter <= (STOPBITVALUE + OFFSET)) { // stopbit
+			//nog iets doen met start- en stopbit?
 		} else { // byte informatie
 			input = (input>>1); // shift input 1 naar rechts, nieuwe bit komt links
-			if (diffcounter >= (BITIS138 - OFFSET) && diffcounter <= (BITIS138 + OFFSET)) { // bit is een 1
+			if (diffcounter >= (BITIS1 - OFFSET) && diffcounter <= (BITIS1 + OFFSET)) { // bit is een 1
 				input |= (1<<7); // zet MSB op 1
-			} else if (diffcounter >= (BITIS038 - OFFSET) && diffcounter <= (BITIS038 + OFFSET)) { // bit is een 0
+			} else if (diffcounter >= (BITIS0 - OFFSET) && diffcounter <= (BITIS0 + OFFSET)) { // bit is een 0
 				input &= ~(1<<7); // zet MSB op 0 (mag weggelaten worden)
 			}
 		}
-		#elif FREQUENCY == 56
-		if (diffcounter >= (STARTBITVALUE56 - OFFSET) && diffcounter <= (STARTBITVALUE56 + OFFSET)) { // startbit
-
-                } else if (diffcounter >= (STOPBITVALUE56 - OFFSET) && diffcounter <= (STOPBITVALUE56 + OFFSET)) { // stopbit
-
-                } else { // byte informatie
-                        input = (input>>1); // shift input 1 naar rechts, nieuwe bit komt links
-                        if (diffcounter >= (BITIS156 - OFFSET) && diffcounter <= (BITIS156 + OFFSET)) { // bit is een 1
-                                input |= (1<<7); // zet MSB op 1
-                        } else if (diffcounter >= (BITIS056 - OFFSET) && diffcounter <= (BITIS056 + OFFSET)) { // bit is een 0
-                                input &= ~(1<<7); // zet MSB op 0 (mag weggelaten worden)
-                        }
-                }
-		#else
-		#pragma GCC error "geen geldige frequentie gekozen"
-		#endif
-
 	} else { // neergaande flank
 		prevcounter = TCNT1; // onthoud counterstand, als het goed is 0, anders dicht in de buurt van 0.
 	}
@@ -99,15 +76,14 @@ void IR_prepare_send(void) {
 	#if FREQUENCY == 38
 	OCR2A = KHZ38; //maximum 1 knipper 38 KHz
 	OCR2B = DUTYCYCLE38;
-	OCR1A = TOTALBIT38; //totaal 1 bit 38 KHz
 	#elif FREQUENCY == 56
 	OCR2A = KHZ56; //maximum 1 knipper 56 KHz
 	OCR2B = DUTYCYCLE56;
-	OCR1A = TOTALBIT56; //totaal 1 bit 56 KHz
 	#else
 	// exception error, geen (geldige) khz gekozen
 	#pragma GCC error "geen geldige frequentie gekozen"
 	#endif
+	OCR1A = TOTALBIT
 
 	/* TIMER1 - Bepaald informatie in signaal */
 	TCCR1A |= (1<<WGM10) | (1<<WGM11); //fast PWM CTC
@@ -128,39 +104,19 @@ void IR_prepare_receive(void) {
 
 
 void IR_send(uint8_t waarde) {
-	#if FREQUENCY == 38
-	OCR1B = STARTBITVALUE38; // start bit
+	OCR1B = STARTBITVALUE; // start bit
 	while(TCNT1 != 0); //wacht tot niewe bit
 	for (int i=0; i<=7; i++) {
 		if (waarde & (1<<i)) { //is bit i 1?
-			OCR1B = BITIS138; //wacht tot nieuwe bit
+			OCR1B = BITIS1; //wacht tot nieuwe bit
 			while(TCNT1 != 0);
 		} else { //bit i is 0
-			OCR1B = BITIS038;
+			OCR1B = BITIS0;
 			while(TCNT1 != 0);
 		}
 	}
-	OCR1B = STOPBITVALUE38; // stop bit
+	OCR1B = STOPBITVALUE; // stop bit
 	while(TCNT1 != 0);
-
-        #elif FREQUENCY == 56
-        OCR1B = STARTBITVALUE56; // start bit
-        while(TCNT1 != 0); //wacht tot niewe bit
-        for (int i=0; i<=7; i++) {
-                if (waarde & (1<<i)) { //is bit i 1?
-                        OCR1B = BITIS156; //wacht tot nieuwe bit
-                        while(TCNT1 != 0);
-                } else { //bit i is 0
-                        OCR1B = BITIS056;
-                        while(TCNT1 != 0);
-                }
-        }
-        OCR1B = STOPBITVALUE56; // stop bit
-        while(TCNT1 != 0);
-
-	#else
-	#pragma GCC error "geen geldige frequentie gekozen"
-	#endif
 }
 
 
@@ -176,7 +132,3 @@ uint8_t getInput(void) {
 }
 
 //======================================================================
-
-uint8_t ontcijfer_input(uint8_t input) { // van tijden naar byte
-	return input;
-}
