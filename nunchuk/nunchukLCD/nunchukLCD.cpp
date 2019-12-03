994421e864d164b8c996f7510e8556942546a631
Simone <simonewijnen290@hotmail.com>
2019-12-03 10:38:27 +0100

    int main
    void setup() en void loop() vervangen door int main()

--- nunchuk/nunchukLCD/nunchukLCD.cpp
+++ nunchuk/nunchukLCD/nunchukLCD.cpp
@@ -1,92 +1,120 @@
-#include <avr/io.h>
+// defines
+#define ADDRESS 0x52
+#define BAUD 9600
+#define TFT_DC 9 // initialisatie LCD
+#define TFT_CS 10 // initialisatie LCD
+#define AANTALLENGTEBREEDTE 9//aantal hokjes in lengte en breedte
+#define LIGHTBROWN 0x7A00
+#define DARKBROWN 0x5980
+#define XUP 10
+#define YUP 50
+#define OBJOFFSET 2
+#define MAXOBJ 8
+
+// includes
 #include <avr/interrupt.h>
-#include <Wire.h>
-#include <Arduino.h>
-#include <Nunchuk.h>
+#include <avr/io.h>
 #include <util/delay.h>
 #include <Adafruit_ILI9341.h> // LCD library
 #include <Adafruit_GFX.h>// LCD library
+#include <Arduino.h> // LCD library
+#include <avr/io.h> // LCD library
 #include <SPI.h>
+#include <Wire.h>
+#include <Nunchuk.h>
 
-#define ADDRESS 0x52
+// global variables
+volatile uint8_t brightness = 0;
+uint8_t lw = 220 / AANTALLENGTEBREEDTE;
 
-#define BAUD 9600
-#define TFT_DC 9 
-#define TFT_CS 10
-
-#define ILI9341_BLACK       0x0000  ///<   0,   0,   0
-#define ILI9341_NAVY        0x000F  ///<   0,   0, 123
-#define ILI9341_DARKGREEN   0x03E0  ///<   0, 125,   0
-#define ILI9341_DARKCYAN    0x03EF  ///<   0, 125, 123
-#define ILI9341_MAROON      0x7800  ///< 123,   0,   0
-#define ILI9341_PURPLE      0x780F  ///< 123,   0, 123
-#define ILI9341_OLIVE       0x7BE0  ///< 123, 125,   0
-#define ILI9341_LIGHTGREY   0xC618  ///< 198, 195, 198
-#define ILI9341_DARKGREY    0x7BEF  ///< 123, 125, 123
-#define ILI9341_BLUE        0x001F  ///<   0,   0, 255
-#define ILI9341_GREEN       0x07E0  ///<   0, 255,   0
-#define ILI9341_CYAN        0x07FF  ///<   0, 255, 255
-#define ILI9341_RED         0xF800  ///< 255,   0,   0
-#define ILI9341_MAGENTA     0xF81F  ///< 255,   0, 255
-#define ILI9341_YELLOW      0xFFE0  ///< 255, 255,   0
-#define ILI9341_WHITE       0xFFFF  ///< 255, 255, 255
-#define ILI9341_ORANGE      0xFD20  ///< 255, 165,   0
-#define ILI9341_GREENYELLOW 0xAFE5  ///< 173, 255,  41
-#define ILI9341_PINK        0xFC18  ///< 255, 130, 198
-#define backgroundColour ILI9341_RED
-
-#define standardPosition tft.fillRect(20, 280, 25, 25, ILI9341_BLACK);
-
-// positions X-axis (to move to the right)
-uint16_t x_positions[9] = {250, 220, 190, 160, 130, 100, 70, 40, 10}; // move to right
-uint16_t clear_x_positions[9] = {250, 220, 190, 160, 130, 100, 70, 40, 10}; // clear positions
-
-// setup LCD hardware
+// use hardware SPI (on Uno, #13, #12, #11) and #10 and #9 for CS/DC
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
-// variables
-volatile unsigned int counter = 0;
-
 // function prototypes
-void moveRight(uint16_t x);
-void clearMoveRight(uint16_t x);
+void nunchuk_init();
+void drawGrid();
+void drawHeartLeft();
+void drawHeartRight();
+void drawPlayer1(uint8_t x, uint8_t y);
+void drawPlayer2(uint8_t x, uint8_t y);
+void drawBomb(uint8_t x, uint8_t y);
+void drawTon(uint8_t x, uint8_t y);
 
-void setup() {
+int main(void) {
 
+  // input & output
   DDRB |= (1 << DDB1) | (1 << DDB2) | (1 << DDB3) | (1 << DDB4) | (1 << DDB5); // TFT scherm
+  DDRC |= (1 << DDC4) | (1 << DDC5); // Nunchuk
 
-  Wire.begin(); // allow I2C communication
-  tft.begin(); // allow SPI communication
-
-  tft.fillScreen(backgroundColour); // background
-
-  // timer1 interrupt
-  TCNT1 = 0;
-  TCCR1A |= 0;
-  TCCR1B |= (1 << CS10) | (1 << CS12) | (1 << WGM12); // 1024 prescaler + set CTC mode
-  OCR1A = 23436.5; // 1.5s * 16000000 / 1024 - 1 = 23436.5 (x sec * (M)Hz / prescaler - 1)
-  TIMSK1 |= (1 << OCIE1A); // enable OCR1A 
-  sei();
-
-  // nunchuk
+  // initializiations
   init();
+  tft.begin(); // enable SPI communication
+  Wire.begin(); // enable TWI communication
+  nunchuk_init(); // start communication between Nunchuk and Arduino
 
-  Nunchuk.begin(ADDRESS); // start communication with Arduino and Nunchuk
-}
+  // enable global interrupts
+  sei();
+  
+  //scherm is 240 * 320 pixels
+  tft.fillScreen(LIGHTBROWN);
 
-ISR(TIMER1_COMPA_vect) {
- 
-  PORTD ^= (1 << PIND4);
-}
+  drawHeartLeft();
+  drawHeartRight();
+  drawGrid();
+  drawPlayer1(8, 0);
+  drawPlayer2(0, 8);
+  drawBomb(4, 2);
 
-void loop() {
+	
+   for(;;){
 
-  Nunchuk.getState(ADDRESS); // retrieve states (defined in .h and .cpp)
-
-  /* X-axis: left: 0 - middle: 127 - right: 255
-     Y-axis: up: 255 - middle: 128 - down: 0
-     Z-button: released: 0 - pressed: 1
-     C-button: released: 0 - pressed: 1 */
+     Nunchuk.getState(ADDRESS); // retrieve states joystick and buttons Nunchuk
 
      
+		
+   }
+
+
+	
+   return 0;
+}
+
+void nunchuk_init() {
+   
+    Nunchuk.begin(ADDRESS); // start communication with Arduino and Nunchuk
+}
+
+void drawGrid() {
+        tft.fillRect(XUP, YUP, AANTALLENGTEBREEDTE * lw, AANTALLENGTEBREEDTE * lw, DARKBROWN);
+        for(int x = 0; x < 9; x++) {
+                for(int y = 0; y < 9; y++) {
+                        tft.drawRect((x * lw) + XUP, (y * lw) + YUP, lw + 1, lw + 1, ILI9341_BLACK);
+                }
+        }
+}
+
+void drawHeartLeft() {
+	tft.fillTriangle(190, 25, 200, 15, 200, 35, ILI9341_RED);
+	tft.fillCircle(203, 30, 5, ILI9341_RED);
+	tft.fillCircle(203, 20, 5, ILI9341_RED);
+}
+
+void drawHeartRight() {
+
+}
+
+void drawPlayer1(uint8_t x, uint8_t y) {
+	tft.fillRect(x * lw + XUP + OBJOFFSET, (y * lw) + YUP + OBJOFFSET, lw - 2 * OBJOFFSET + 1, lw - 2 * OBJOFFSET + 1, ILI9341_CYAN);
+}
+
+void drawPlayer2(uint8_t x, uint8_t y) {
+	tft.fillRect(x * lw + XUP + OBJOFFSET, (y * lw) + YUP + OBJOFFSET, lw - 2 * OBJOFFSET + 1, lw - 2 * OBJOFFSET + 1, ILI9341_RED);
+}
+
+void drawBomb(uint8_t x, uint8_t y) {
+	tft.fillCircle(x * lw + XUP + (0.3 * lw), y * lw + YUP + (0.3 * lw), 5, ILI9341_BLACK);
+}
+
+void drawTon(uint8_t x, uint8_t y) {
+	
 }
